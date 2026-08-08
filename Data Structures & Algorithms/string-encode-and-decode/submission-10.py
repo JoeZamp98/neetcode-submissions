dc092c4196a17f001af39c4f5df51c3c5686c839
JoeZamp98 <42157127+JoeZamp98@users.noreply.github.com>
2026-08-08 10:00:10 -0500

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        return ".".join([str(len(s)) for s in strs]) + "#" + "".join(strs)
+
+
+    def decode(self, s: str) -> List[str]:
+
+        counts, string = s.split("#", maxsplit=1)
+
+        if len(string) == 0:
+
+            if len(counts) == 0:
+                return []
+
+            if len(counts) == 1:
+                return [""]
+
+        counts = counts.split(".")
+
+        position = 0
+        all_substrings = []
+
+        for x in counts:
+
+            sub_str = string[position: position + int(x)]
+            position += int(x)
+            all_substrings.append(sub_str)
+
+        return all_substrings
