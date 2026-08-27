19fd76deaa07bde3bb841e288ea5b7eb3531b043
JoeZamp98 <42157127+JoeZamp98@users.noreply.github.com>
2026-08-27 10:10:09 -0500

    Add: minimum-stack - submission-24

---
+++ Data Structures & Algorithms/minimum-stack/submission-24.py
@@ -0,0 +1,43 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        Set this up so the stack appends to the final position, but min_values appends to the first position.  This was more intuitive in my mind, but I believe they could also run in identical directions if need be.  
+        """
+
+        self.stack = []
+
+        self.min_values = [None]
+        
+    def push(self, val: int) -> None:   
+
+        # Push to stack
+
+        self.stack.append(val)
+
+        # Update minimum value log
+
+        current_min = self.min_values[0]
+
+        if current_min is not None:
+            if current_min < val:
+                self.min_values.insert(0, current_min)
+            else:
+                self.min_values.insert(0, val)
+        else:
+            self.min_values.insert(0, val)
+
+    def pop(self) -> None:
+        
+        self.stack.pop()
+
+        del self.min_values[0]
+
+    def top(self) -> int:
+        
+        return self.stack[-1]
+
+
+    def getMin(self) -> int:
+        
+        return self.min_values[0]
