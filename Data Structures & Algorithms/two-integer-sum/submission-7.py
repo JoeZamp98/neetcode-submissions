0e4dbdc85fda7a24cdbce124e21d9299b40da6f1
JoeZamp98 <42157127+JoeZamp98@users.noreply.github.com>
2026-09-07 13:07:47 -0500

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        all_seen_nums = {} #idx, num
+
+        for idx, x in enumerate(nums):
+
+            all_seen_nums[idx] = x
+
+            difference = target - x
+
+            possible_pair = {k: v for k, v in all_seen_nums.items() if v == difference}
+
+            if possible_pair:
+
+                diff_idx = [k for k, v in possible_pair.items()][0]
+                diff_value = [v for k, v in possible_pair.items()][0]
+
+                if diff_value != x:
+
+                    return [diff_idx, idx]
+
+        return []
+
+
+        
